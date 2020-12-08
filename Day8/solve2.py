45bb4559fb9874421a652b7995d552936c47a25e
rachwalk <kajetan.rachwal@mailfence.com>
2020-12-08 13:27:16 +0100

    Day 8 part 2

--- Day8/solve2.py
+++ Day8/solve2.py
@@ -10,25 +10,52 @@ class Instruction:
 instructions = []
 
 
+def reset(inst):
+    for e in inst:
+        e.visited = False
+
+
 for line in f.readlines():
     x = line.rstrip().split()
     instructions.append(Instruction(x[0], int(x[1])))
 
-accumulator = 0
-ptr = 0
-while(True):
-    #check if loop
-    if(instructions[ptr].visited):
-        break
-    else:
-        instructions[ptr].visited = True
+def runProgram(instructions):
+    accumulator = 0
+    ptr = 0
+    while(True):
+        #check if loop
+        if(ptr == len(instructions)):
+            reset(instructions)
+            return((accumulator, True))
+        if(instructions[ptr].visited):
+            reset(instructions)
+            return((accumulator, False))
+        else:
+            instructions[ptr].visited = True
+    
+        if(instructions[ptr].inst == "nop"):
+            ptr = ptr + 1
+        elif(instructions[ptr].inst == "acc"):
+            accumulator = accumulator + instructions[ptr].val
+            ptr = ptr + 1
+        else:
+            ptr = ptr + instructions[ptr].val
 
-    if(instructions[ptr].inst == "nop"):
-        ptr = ptr + 1
-    elif(instructions[ptr].inst == "acc"):
-        accumulator = accumulator + instructions[ptr].val
-        ptr = ptr + 1
-    else:
-        ptr = ptr + instructions[ptr].val
 
-print(accumulator)
+for e in instructions:
+    if(e.inst == "nop"):
+        e.inst = "jmp"
+        x = runProgram(instructions)
+        if(x[1]):
+            print(x[0])
+        e.inst = "nop"
+
+
+for e in instructions:
+    if(e.inst == "jmp"):
+        e.inst = "nop"
+        x = runProgram(instructions)
+        if(x[1]):
+            print(x[0])
+        e.inst = "jmp"
+
